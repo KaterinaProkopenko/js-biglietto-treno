770f68c798d299e7c00075c19394935618380cd6
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-02-22 17:52:37 +0100

    L'esercizio è finito

--- js/biglietto.js
+++ js/biglietto.js
@@ -1,5 +1,6 @@
 const kilometer = prompt("Quanti chilometri vuoi percorrere?");
 console.log(kilometer);
+document.getElementById('kilometer').innerHTML += kilometer + ' km';
 
 const age = prompt("Quanti anni hai?");
 console.log(age);
@@ -14,9 +15,12 @@ let priceMinorenni = price - ((price * 20) / 100);
 let priceOverSessanta = price - ((price * 40) / 100);
 
 if (age < 18) {
-    console.log(priceMinorenni);
+    console.log(priceMinorenni.toFixed(2));
+    document.getElementById('total-price').innerHTML += priceMinorenni.toFixed(2) + ' Euro';
 } else if (age > 65) {
-    console.log(priceOverSessanta);
+    console.log(priceOverSessanta.toFixed(2));
+    document.getElementById('total-price').innerHTML += priceOverSessanta.toFixed(2) + ' Euro';
 } else {
     console.log(price);
+    document.getElementById('total-price').innerHTML += price.toFixed(2) + ' Euro';
 }
